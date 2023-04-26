7e5274ab9e8295e6fd52c60dccd92fc4017399d2
Michal Vlasák <lahcim8@gmail.com>
2023-04-26 08:07:11 +0200

    Update module.c

--- module.c
+++ module.c
@@ -1,10 +1,10 @@
 #include <lualib.h>
 
 int
-luaopen_mylib(lua_State *L)
+luaopen_mylib(lua_State *L)             // [...]
 {
-	lua_createtable(L, 0, 1);
-	lua_pushliteral(L, "myvalue");
-	lua_setfield(L, -2, "mykey");
+	lua_createtable(L, 0, 1);       // [...] {}
+	lua_pushliteral(L, "myvalue");  // [...] {} "myvalue"
+	lua_setfield(L, -2, "mykey");   // [...] {myvalue="mykey"}
 	return 1;
 }
